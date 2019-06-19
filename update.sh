3a078e14f699f882d7fda949836973debeb3f836
Nye Liu <nye@blockdaemon.com>
2019-06-19 22:53:42 +0000

    so we can . update.sh instead of ./update.sh if we want

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd files/bin
+pushd files/bin
 curl -LO https://harmony.one/node.sh
 chmod +x node.sh
 echo asdf | fakeroot ./node.sh -1 -k asdf <> /dev/null 2>&1
 git diff harmony-checksums.txt
+popd
