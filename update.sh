2d0f6bc7cd665f39806d5d31280c26178be13c2f
Nye Liu <nye@blockdaemon.com>
2019-06-22 22:19:10 +0000

    Add wallet and mystatus.sh, change verbosity to info

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 pushd files/bin
-curl -LO https://harmony.one/node.sh
-chmod +x node.sh
+for i in node wallet mystatus; do
+  curl -LO https://harmony.one/$i.sh
+  chmod +x $i.sh
+done
+./wallet.sh -d
 echo asdf | fakeroot ./node.sh -1 -k asdf <> /dev/null 2>&1
 git diff harmony-checksums.txt
 popd
