125ea32521e38939c3dbfb85cd7a825b5046ea5e
Nye Liu <nye@blockdaemon.com>
2019-06-23 23:43:37 +0000

    Don't page on git diff

--- update.sh
+++ update.sh
@@ -6,5 +6,5 @@ for i in node wallet mystatus; do
 done
 ./wallet.sh -d
 echo asdf | ./node.sh -S -1 -k asdf <> /dev/null 2>&1
-git diff harmony-checksums.txt
+git -P diff harmony-checksums.txt
 popd
