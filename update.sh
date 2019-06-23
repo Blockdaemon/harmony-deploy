05456495389697a54cac7fc2cf5647cd647d5e0c
Nye Liu <nye@blockdaemon.com>
2019-06-23 23:57:59 +0000

    Chmod local bins, preserve when copying

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ for i in node wallet mystatus; do
   curl -SsLO https://harmony.one/$i.sh
   chmod +x $i.sh
 done
+chmod +x wallet harmony
 ./wallet.sh -d
 echo asdf | ./node.sh -S -1 -k asdf <> /dev/null 2>&1
 git -P diff harmony-checksums.txt
