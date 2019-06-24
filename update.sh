7a5a0566c9c42e02101f3d5801f6b0d9dc456467
Nye Liu <nye@blockdaemon.com>
2019-06-24 00:54:44 +0000

    Handle AWS issues gracefully

--- update.sh
+++ update.sh
@@ -4,8 +4,14 @@ for i in node wallet mystatus; do
   curl -SsLO https://harmony.one/$i.sh
   chmod +x $i.sh
 done
-chmod +x wallet harmony
-./wallet.sh -d
+./wallet.sh -d <> /dev/null 2>&1
+if dd if=wallet count=1 bs=300 | grep Error; then
+   rm wallet
+fi
 echo asdf | ./node.sh -S -1 -k asdf <> /dev/null 2>&1
+chmod +x wallet harmony
+if dd if=harmony count=1 bs=300 | grep Error; then
+   rm harmony
+fi
 git -P diff harmony-checksums.txt
 popd
