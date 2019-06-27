b0c100f5529d5857ef348c20f573fb32e3068afa
Nye Liu <nye@blockdaemon.com>
2019-06-27 01:14:09 +0000

    Find the things we want in "account"

--- top10.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -z "$1" ]; then
-shard="$1 "
-else
-shard=""
-fi
-curl -Ss https://harmony.one/balances | cut -f 2 -d \' | sed '/.*gn-keys.*/q' | grep ^one | sort -rnk 4 | grep " $shard" | head
