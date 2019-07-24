82e8062a942100baed7d67f2282a41b29f229507
Nye Liu <nye@blockdaemon.com>
2019-07-24 06:47:46 +0000

    validate md5sums

--- update.sh
+++ update.sh
@@ -13,5 +13,16 @@ chmod +x wallet harmony
 if dd if=harmony count=1 bs=300 | grep Error; then
    rm harmony
 fi
+
 git -P diff harmony-checksums.txt
+
+while read -r line; do
+    file=$(echo "$line" | cut -f 1 -d \) | cut -f 2 -d \()
+    sum1=$(echo "$line" | cut -f 2 -d ' ')
+    sum2=$(sha256sum $file | cut -f 1 -d " ")
+    if [ "$sum1" != "$sum2" ]; then
+       echo "$file '$sum1'!='$sum2'"
+    fi
+done < harmony-checksums.txt
+
 popd
