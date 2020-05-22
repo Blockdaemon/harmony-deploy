2d7b33998cf4339a5e0000a624d15329073a9ffa
Nye Liu <nye@blockdaemon.com>
2020-05-22 06:33:33 +0000

    normalize to ONE

--- scripts/auction.js
+++ scripts/auction.js
@@ -1,4 +1,4 @@
 [.result["epos-slot-candidates"][] |
 {"s": .["stake-per-key"], "v": .validator, "k": .["keys-at-auction"][]}] |
 sort_by(.s) | reverse |
-to_entries | map([(.key + 1), .value.v]) | .[]
+to_entries | map([(.key + 1), .value.v, (.value.s/1e+18)]) | .[]
