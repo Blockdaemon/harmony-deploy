4af4a5dd0abf4787b73899ba02973e938db880b3
Nye Liu <nye@blockdaemon.com>
2020-05-22 08:22:04 +0000

    cosmetic, remove double double quotes

--- scripts/auction.sh
+++ scripts/auction.sh
@@ -6,7 +6,7 @@ HMY=${TOP}/files/bin/hmy
 if [ $# -gt 0 -a -r "$1" ]; then
     sed=()
     for i in `cat $1`; do
-	n=$(${HMY} -n https://api.s0.t.hmny.io blockchain validator information $i | jq .result.validator.name)
+	n=$(${HMY} -n https://api.s0.t.hmny.io blockchain validator information $i | jq -r .result.validator.name)
 	sed+=("s/$i/$n/")
     done
     grep=$(tr '\n' ' ' < $1 | sed 's/ $//' | sed 's/ /\\|/g')
