7e3a4aeba4b9306a85ffc50a2bacf2aa4b8798a3
Nye Liu <nye@blockdaemon.com>
2020-05-22 03:41:20 +0000

    Put the name of our validator in :)

--- scripts/auction.sh
+++ scripts/auction.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 if [ $# -gt 0 -a -r "$1" ]; then
-    regex=$(tr '\n' ' ' < $1 | sed 's/ $//' | sed 's/ /\\|/g')
+    sed=()
+    for i in `cat $1`; do
+	n=$(hmy -n https://api.s0.t.hmny.io blockchain validator information $i | jq .result.validator.name)
+	sed+=("s/$i/$n/")
+    done
+    grep=$(tr '\n' ' ' < $1 | sed 's/ $//' | sed 's/ /\\|/g')
+fi
+if [ "${#sed[@]}" -gt 0 ]; then
+    sed=$( IFS=\; ; echo "${sed[*]}")
 fi
 
-hmy -n https://api.s0.t.hmny.io blockchain median-stake | jq -cr -f auction.js | grep "$regex"
+hmy -n https://api.s0.t.hmny.io blockchain median-stake | jq -cr -f auction.js | grep "$grep" | sed -e "$sed"
