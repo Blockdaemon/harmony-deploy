9d33a63cdec406f30ff5cdeb5b01a6fb4c5050eb
Nye Liu <nye@blockdaemon.com>
2020-05-22 08:19:42 +0000

    Find our hmy

--- scripts/auction.sh
+++ scripts/auction.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+TOP=$(realpath $(dirname $0)/..)
+HMY=${TOP}/files/bin/hmy
+
 if [ $# -gt 0 -a -r "$1" ]; then
     sed=()
     for i in `cat $1`; do
-	n=$(hmy -n https://api.s0.t.hmny.io blockchain validator information $i | jq .result.validator.name)
+	n=$(${HMY} -n https://api.s0.t.hmny.io blockchain validator information $i | jq .result.validator.name)
 	sed+=("s/$i/$n/")
     done
     grep=$(tr '\n' ' ' < $1 | sed 's/ $//' | sed 's/ /\\|/g')
@@ -12,4 +15,4 @@ if [ "${#sed[@]}" -gt 0 ]; then
     sed=$( IFS=\; ; echo "${sed[*]}")
 fi
 
-hmy -n https://api.s0.t.hmny.io blockchain median-stake | jq -cr -f auction.js | grep "$grep" | sed -e "$sed"
+${HMY} -n https://api.s0.t.hmny.io blockchain median-stake | jq -cr -f auction.js | grep "$grep" | sed -e "$sed"
