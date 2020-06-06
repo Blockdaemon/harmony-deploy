3557954c59239e22d9e01ad595a3c46db68fde6d
Nye Liu <nye@blockdaemon.com>
2020-06-05 18:20:54 -0700

    Add delegated tools

---
+++ scripts/get-delegated-by-delegator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. $(dirname $0)/functions
+
+a=accounts
+if [ $# -gt 0 ]; then a=$1; fi
+
+for i in `cat $a`; do
+    ${HMY} -n https://api.s0.t.hmny.io blockchain delegation by-delegator $i | \
+    jq -c '.result | select(length>0) |
+	map ( select(.amount>0) |
+	    {(.["delegator_address"]):(.amount/1e18)}
+	) | select(length>0) // empty
+    '
+done
---
+++ scripts/get-delegated-by-validator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. $(dirname $0)/functions
+
+a=accounts
+if [ $# -gt 0 ]; then a=$1; fi
+
+for i in `cat $a`; do
+    ${HMY} -n https://api.s0.t.hmny.io blockchain validator information $i | \
+    jq '.result.validator |
+    { (.address): .delegations |
+	map( select(.amount>0) |
+	    {(.["delegator-address"]):(.amount/1e18)}
+	) | add
+    } | del(.[]|nulls) | select(length>0)
+    '
+done
