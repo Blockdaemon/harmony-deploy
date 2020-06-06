5e864f54959cbd357823a4f0589882a0851636aa
Nye Liu <nye@blockdaemon.com>
2020-06-05 19:11:56 -0700

    print validator on delegated-by-delegator

--- scripts/get-delegated-by-delegator.sh
+++ scripts/get-delegated-by-delegator.sh
@@ -8,8 +8,9 @@ if [ $# -gt 0 ]; then a=$1; fi
 for i in `cat $a`; do
     ${HMY} -n https://api.s0.t.hmny.io blockchain delegation by-delegator $i | \
     jq -c '.result | select(length>0) |
-	map ( select(.amount>0) |
-	    {(.["delegator_address"]):(.amount/1e18)}
+	map ( select(.amount>0) | {
+		(.["delegator_address"]): {(.["validator_address"]):(.amount/1e18)}
+	    }
 	) | select(length>0) // empty
     '
 done
