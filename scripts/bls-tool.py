e21ed722c8a8ddd253a18476b86e3b61d5a9ab46
Nye Liu <nye@blockdaemon.com>
2020-06-07 21:14:30 -0700

    add eff calculator

--- scripts/bls-tool.py
+++ scripts/bls-tool.py
@@ -34,6 +34,16 @@ def get_hmy_validator_information(addr, url=None):
         return
     return resp['result']
 
+def get_eff_stake(info):
+    ret = 0
+    if info['metrics'] and 'by-bls-key' in info['metrics']:
+        for m in info['metrics']['by-bls-key']:
+            if 'key' in m and 'effective-stake' in m['key']:
+                eff = float(m['key']['effective-stake'])
+                if eff > ret:
+                    ret = eff
+    return ret
+
 def main():
     with open("bls-tool.yaml") as file:
         addrs = yaml.load(file, Loader=yaml.FullLoader)
@@ -50,24 +60,30 @@ def main():
             info = get_hmy_validator_information(v)
             name = info['validator']['name']
             candidates[i]['name'] = name
+            candidates[i]['eff'] = get_eff_stake(info)
             if addrs[v]:
                 new_len = addrs[v]['try_bls']
                 if old_len != new_len:
                     old_stake = candidates[i]['stake-per-key']
                     new_stake = c['stake']/new_len
-                    print_err(f"{name}: {old_stake/1E18} x{old_len} -> {new_stake/1E18} x{new_len}")
+                    print_err(f"{name}: {round(old_stake/1E24,2)}M x{old_len} -> {round(new_stake/1E24,2)}M x{new_len}")
                     candidates[i]['len'] = new_len
                     candidates[i]['stake-per-key'] = new_stake
     candidates = sorted(candidates, key = lambda i: i['stake-per-key'],reverse=True)
+    max_eff = get_eff_stake(get_hmy_validator_information(candidates[0]['validator']))
+    print(f"med/max: {round(median/1E24,2)}M/{round(max_eff/1E24,2)}M ({round(max_eff*100.0/median)}%)")
     i = 1
     for c in candidates:
         l = c['len']
         if 'name' in c:
             pct_of_median = round(c['stake-per-key']*100.0/median)
+            s = f"{c['name']} {round(c['stake-per-key']/1E24,2)}M ({pct_of_median}%), {round(c['eff']/1E24,2)}M"
             if l>1:
-                print(f"{i}-{i+l-1}: {c['name']} ({pct_of_median}%) x{l}")
+                p = f"{i}-{i+l-1}".rjust(7)
+                print(f"{p}: {s}x{l}")
             else:
-                print(f"  {i}: {c['name']} ({pct_of_median}%)")
+                p = f"{i}".rjust(7)
+                print(f"{p}: {s}")
         i += l
 
 if __name__ == "__main__":
